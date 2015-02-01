24a1dc66f33784dcf53fcd7560c3b8127d1d9ea4
Junyu Wang <junyuw@berkeley.edu>
2015-02-01 01:22:03 -0800

    group topic index page finished

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -12,8 +12,8 @@ class Topic < ActiveRecord::Base
 
   validates :content, presence: true, length:
     { 
-     minimum: 1, 
-     maximum: 200,
+     minimum: 10, 
+     maximum: 50,
      tokenizer: lambda { |str| str.split(/\s+/) },
      too_short: "must have at least %{count} words",
      too_long: "must have at most %{count} words"
