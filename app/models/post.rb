e8d8b841bd60e831f8de43d4a59a4915a6d63a20
Junyu Wang <junyuw@berkeley.edu>
2015-02-11 01:38:53 -0800

    limited post title to 15 words

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,15 +6,15 @@ class Post < ActiveRecord::Base
   validates :summary, presence: true, length:
     {
       minimum: 1,
-      maximum: 20,
+      maximum: 15,
       tokenizer: lambda { |str| str.split(/\s+/) },
       too_short: "must have at least %{count} words",
       too_long: "must have at most %{count} words"
     };
   validates :content, presence: true, length:
     {
-      minimum: 1,
-      maximum: 500,
+      minimum: 5,
+      maximum: 300,
       tokenizer: lambda { |str| str.split(/\s+/) },
       too_short: "must have at least %{count} words",
       too_long: "must have at most %{count} words"
